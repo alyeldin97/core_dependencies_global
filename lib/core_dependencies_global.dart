22e080fc7396cad6907714f8c4a8457ab3b1dc3e
sameh <samehmohamedali5@gmail.com>
2025-01-06 18:19:31 +0200

    feat:navigation service

--- lib/core_dependencies_global.dart
+++ lib/core_dependencies_global.dart
@@ -1,5 +1,6 @@
 // Services
 export 'services/network_service.dart';
+export 'services/navigation/navigation_service.dart';
 
 // Utilities
 export 'ui_utilities/sized_boxes.dart';
@@ -8,5 +9,9 @@ export 'ui_utilities/loading.dart';
 export 'ui_utilities/images.dart';
 export 'ui_utilities/drop_down_button.dart';
 export 'ui_utilities/divider.dart';
+export 'ui_utilities/text_field.dart';
+export 'ui_utilities/dialog.dart';
+
+
 
 
