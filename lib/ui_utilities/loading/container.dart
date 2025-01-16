8b1a396631a5bfb2c3df7faf89f0010de460e8ed
sameh <samehmohamedali5@gmail.com>
2025-01-16 13:59:09 +0200

    feat:add loading listview and gridview

---
+++ lib/ui_utilities/loading/container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../app_container.dart';
+
+class LoadingContainer extends StatelessWidget {
+  const LoadingContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      child: const AppContainer(
+        width: double.infinity,
+        height: 100,
+        borderRadius: 10,
+        isElevated: true,
+      ),
+    );
+  }
+}
---
+++ lib/ui_utilities/loading/listview.dart
@@ -0,0 +1,48 @@
+import 'package:core_dependencies_global/ui_utilities/sized_boxes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../app_container.dart';
+
+class LoadingListview extends StatelessWidget {
+  const LoadingListview({
+    super.key,
+    this.height,
+    this.scrollDirection,
+    this.heightContainer,
+    this.widthContainer,
+    this.separatorBuilder,
+  });
+
+  final double? height;
+  final Axis? scrollDirection;
+  final double? heightContainer;
+  final double? widthContainer;
+  final Widget? separatorBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height ?? 210.h,
+      child: Shimmer.fromColors(
+        baseColor: Colors.grey[300]!,
+        highlightColor: Colors.grey[100]!,
+        child: ListView.separated(
+          scrollDirection: scrollDirection ?? Axis.horizontal,
+          shrinkWrap: true,
+          itemCount: 6,
+          separatorBuilder: (context, index) => separatorBuilder ?? 20.widthBox___________________________(),
+          itemBuilder: (context, index) {
+            return AppContainer(
+              width: widthContainer ?? 150,
+              height: heightContainer ?? 120,
+              borderRadius: 10,
+              isElevated: true,
+            ); // replace this with your actual loading widget
+          },
+        ),
+      ),
+    );
+  }
+}
