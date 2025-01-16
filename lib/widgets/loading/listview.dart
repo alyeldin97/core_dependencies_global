17c2e1df85da53ba9a9a926d048bd9092b9adf52
sameh <samehmohamedali5@gmail.com>
2025-01-16 22:55:37 +0200

    feat:add loading listview and gridview

--- lib/widgets/loading/listview.dart
+++ lib/widgets/loading/listview.dart
@@ -1,4 +1,4 @@
-import 'package:core_dependencies_global/ui_utilities/sized_boxes.dart';
+ import 'package:core_dependencies_global/core_dependencies_global.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shimmer/shimmer.dart';
