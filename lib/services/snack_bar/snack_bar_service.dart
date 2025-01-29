f371f60ca5c36c081cd183db2715d2caf4997dd3
Aly Eldin Muhammad <85060215+alyeldin97@users.noreply.github.com>
2025-01-29 03:56:44 +0200

    refactor: snackbar service

--- lib/services/snack_bar/snack_bar_service.dart
+++ lib/services/snack_bar/snack_bar_service.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/snackbar/snackbar.dart';
 
 abstract class SnackBarService {
-  void showSnackBar(String message, Color? backgroundColor, Color? textColor,
-      SnackPosition? position, String? title);
+  void showSnackBar(
+      {String? message,
+      Color? backgroundColor,
+      Color? textColor,
+      SnackPosition? position,
+      String? title,
+      Duration? duration});
 }
 
 class SnackBarImpl implements SnackBarService {
+  final String? title;
+  final String message;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final SnackPosition? position;
+  final Duration? duration;
+
+  SnackBarImpl(
+      {required this.title,
+      required this.message,
+      required this.backgroundColor,
+      required this.textColor,
+      required this.position,
+      required this.duration});
+
   @override
-  void showSnackBar(String message, Color? backgroundColor, Color? textColor,
-      SnackPosition? position, String? title) {
+  void showSnackBar(
+      {String? message,
+      Color? backgroundColor,
+      Color? textColor,
+      SnackPosition? position,
+      String? title,
+      Duration? duration}) {
     Get.snackbar(
-      title ?? 'Success',
-      message,
-      snackPosition: position ?? SnackPosition.BOTTOM,
-      backgroundColor: backgroundColor ?? Colors.green,
-      colorText: textColor ?? Colors.white,
-      duration: Duration(seconds: 3),
+      title ?? this.title ?? "",
+      message ?? this.message,
+      snackPosition: position ?? this.position ?? SnackPosition.BOTTOM,
+      backgroundColor: backgroundColor ?? this.backgroundColor ?? Colors.green,
+      colorText: textColor ?? this.textColor ?? Colors.white,
+      duration: duration ?? this.duration ?? Duration(seconds: 3),
     );
   }
 }
