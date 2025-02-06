f35df255c8389fa8efc76fda2bac2dd910dae7b3
sameh <samehmohamedali5@gmail.com>
2025-02-06 02:19:32 +0200

    feat:add snackBar service

--- lib/services/snack_bar/snack_bar_service.dart
+++ lib/services/snack_bar/snack_bar_service.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
- 
 
 abstract class SnackBarService {
-  void showSnackBar(
-      {String? message,
-      Color? backgroundColor,
-      Color? textColor,
-      SnackPosition? position,
-      String? title,
-      Duration? duration});
+  void showSnackBar({
+    String? message,
+    Color? backgroundColor,
+    Color? textColor,
+    SnackPosition? position,
+    String? title,
+    Duration? duration,
+  });
 }
- 
